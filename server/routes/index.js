453c92eeab76605993abb61918fed01286bfa889
ejima_xx <060812ez.tm@gmail.com>
2024-07-15 11:09:39 +0900

    ボード・処理情報html表示のための変更

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+let datas = [];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', contents: datas.length > 0 ? datas : "" });
 });
+
+router.post("/", (req, res, next) => {
+  console.log(req.body);
+  datas.push(JSON.stringify(req.body));
+  res.redirect("/");
+})
 
 module.exports = router;
