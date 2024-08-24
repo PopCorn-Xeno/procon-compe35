dd1e98f3c3226777cea65d2d50907e3e6b6f6212
ejima_xx <060812ez.tm@gmail.com>
2024-08-24 12:00:20 +0900

    main.jsの出力を通信して簡素にブラウザ上に表示する

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,17 +1,49 @@
 var express = require('express');
 var router = express.Router();
+const { exec, spawn } = require("child_process");
 
 let datas = [];
+let log = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { datas: datas.length > 0 ? datas : "" });
+  res.render('index', { datas: datas.length > 0 ? datas : "", log: log });
 });
 
+router.get("/start", (req, res, next) => {
+  // exec("node base/main.js", (err, stdout, stderr) => {
+  //   if (err) {
+  //     console.error(`stderr: ${stderr}`);
+  //     return;
+  //   }
+  //   log = stdout;
+  //   console.log(log);
+  //   res.redirect("../");
+  // })
+  const child = spawn("node", ["--max-old-space-size=16000", "base/main.js"]);
+
+  child.stdout.on("data", data => {
+    console.log(data.toString());
+    // log.push(data.toString() + "\n");
+    res.write(data.toString() + "\n");
+  });
+
+  child.stderr.on("data", data => {
+    console.error(data.toString());
+    res.write(data.toString() + "\n");
+  });
+
+  child.on("close", code => {
+    res.end();
+  })
+
+  // res.redirect("../");
+})
+
 router.post("/", (req, res, next) => {
-  console.log(req.body.array);
-  datas.push(req.body);
-  res.redirect("/");
+  // console.log(req.body.array);
+  // datas.push(req.body);
+  // res.redirect("/");
 })
 
 module.exports = router;
