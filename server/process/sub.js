1ea94cb6b28d9ab847a44c6c297864c9065efc22
ejima_xx <060812ez.tm@gmail.com>
2024-08-31 13:39:07 +0900

    パラメータ名の変更

--- server/process/sub.js
+++ server/process/sub.js
@@ -9,8 +9,9 @@ const boardData = new BoardData(null, null, process.argv[2], process.argv[3]);
 
 boardData.answer.allSort(true);
 
-boardData.answer.makeSendData(true, (path, count) => console.log(path, count));
+boardData.answer.makeSendData(true, (filename, count) => console.log(filename, count));
 
+// throw new Error("test error");
 /* コマンドライン引数のシグネチャ
  * node --max-old-space-size={RAM_SIZE[MB]} sub.js width height
  * width: process.argv[2]
