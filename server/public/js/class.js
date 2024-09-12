bc48663674feee321e4a0fd837acd7ada8885ba7
ejima_xx <060812ez.tm@gmail.com>
2024-09-12 11:35:30 +0900

    機能の追加 fetch部分の汎用化 regionディレクティブの利用

--- server/public/js/class.js
+++ server/public/js/class.js
@@ -31,7 +31,7 @@ export class StopWatch {
     }
 
     /**
-     * ストップウォッチ表示を作成する
+     * ストップウォッチ表示を作成する (`private method`)
      * @param {number} seconds 秒 
      * @returns `00:00.00`表示の文字列
      */
@@ -55,23 +55,35 @@ export class StopWatch {
         return `${fillZero(min)}:${fillZero(sec)}.${fillZero(ms)}`;
     }
 
+    /**
+     * ストップウォッチ表示を作成する
+     * @param {number} seconds 秒 
+     */
+    display(seconds) {
+        this.outputElement.textContent = this.#display(seconds)
+        return this;
+    }
+
     /** ストップウォッチを開始する */
     start() {
         this.#id = setInterval(() => {
             this.#time += this.#increment;
             this.outputElement.textContent = this.#display(this.#time);
         }, this.#increment * 1000);
+        return this;
     }
 
     /** ストップウォッチを停止する */
     stop() {
         clearInterval(this.#id);
+        return this;
     }
 
     /** ストップウォッチをリセットする */
     reset() {
         this.#time = 0;
         this.outputElement.textContent = this.#display(0);
+        return this;
     }
 }
 
@@ -236,6 +248,7 @@ export class ProgressBar {
     constructor(target) {
         this.#gauge = target.querySelector(".gauge");
         this.#percentage = target.querySelector(".percentage");
+        this.reset();
     }
 
     /**
@@ -243,8 +256,149 @@ export class ProgressBar {
      * @param {number} ratio 1以下の正の小数
      */
     progress(ratio) {
+        const fillZero = strValue => strValue.length <= 2 ? strValue + ".0" : strValue;
         let percent = Math.round(ratio * 100 * 10) / 10;
         this.#gauge.style.width = `${percent}%`;
-        this.#percentage.textContent = percent;
+        this.#percentage.textContent = fillZero(percent.toString());
+    }
+
+    /** リセットする */
+    reset() {
+        this.#gauge.style.width = "0%";
+        this.#percentage.textContent = "0.0"
+    }
+}
+
+/**
+ * 現在の処理ステータスを表示させるクラス
+ */
+export class Status {
+    /**
+     * @param {HTMLElement} target `<div id="status">`
+     */
+    constructor(target) {
+        this.title = target.querySelector(".status-title");
+        this.subtitle = target.querySelector(".status-subtitle");
+    }
+
+    /**
+     * 表示を変える
+     * @param {string} title 日本語タイトル
+     * @param {string} subtitle 英語サブタイトル
+     * @returns インスタンス
+     */
+    #change(title, subtitle) {
+        this.title.textContent = title;
+        this.subtitle.textContent = subtitle;
+        return this;
+    }
+    /** スタンバイ */
+    standBy() {
+        return this.#change("実行待機", "STAND-BY");
+    }
+    /** 処理中 */
+    process() {
+        return this.#change("処理中", "PROCESS");
+    }
+    /** 処理完了 */
+    complete() {
+        return this.#change("処理完了", "COMPLETE");
+    }    
+    /** エラー終了 */
+    error() {
+         return this.#change("異常終了", "ERROR");
+    }
+}
+
+/**
+ * コンソールウィンドウを使うためのクラス
+ */
+export class ConsoleWindow {
+    /**
+     * @type {HTMLElement}
+     */
+    #console;
+    #id = 0;
+    isScrolled = false;
+
+    constructor(target) {
+        this.#console = target;
+
+        // コンソールがスクロールされているかどうか判定するイベントを登録する
+        this.#console.addEventListener("scroll", () => {
+            this.isScrolled = true;
+            clearTimeout(this.#id);
+            // 250msごとにスクロールされているか否か判断する
+            this.#id = setTimeout(() => {
+                this.isScrolled = false;
+            }, 250);
+        });
+    }
+    
+    /**
+     * 子要素を追加してコンソール出力を再現する
+     * @param {string} className 付与するクラス名
+     * @param  {...any} data データ - 配列としてわたすと同じ段落で要素ごとに改行する
+     */
+    #append(className, ...data) {
+        data.forEach(message => {
+            let element = document.createElement("li");
+            // 配列で渡された場合に、同じ段落内で改行させる
+            if (Array.isArray(message)) {
+                message.forEach(m => element.innerHTML += `${m}<br>`);
+            }
+            else {
+                element.innerHTML = message;
+            }
+            element.classList.add(className);
+            element = this.#console.appendChild(element);
+            //  スクロールしていないときだけ下部に強制スクロール
+            if (!this.isScrolled) {
+                element.scrollIntoView({
+                    block: "end",
+                    inline: "nearest",
+                    behavior: "smooth"
+                });
+            }
+        });
+    }
+
+    /**
+     * 通常ログを出力する
+     * @param  {...any} data 出力データ - 配列としてわたすと同じ段落で要素ごとに改行する
+     */
+    log(...data) {
+        this.#append("log", ...data);
+    }
+
+    /**
+     * エラーログを出力する
+     * @param  {...any} data 出力データ - 配列としてわたすと同じ段落で要素ごとに改行する
+     */
+    error(...data) {
+        this.#append("error", ...data);
+    }
+}
+
+/**
+ * 処理結果を保存するクラス
+ */
+export class Result {
+    constructor(matchValue = undefined, id = "", orderCount = 0) {
+        /**
+         * 現在のセルの一致数
+         * @type {number | undefined} 
+         */
+        this.matchValue = matchValue;
+        /**
+         * 処理結果(送信データ)の出力ファイル名
+         * @type {string} 
+         */
+        this.id = id;
+        /**
+         * 操作手数
+         * @type {number} 
+         */
+        this.orderCount = orderCount;
     }
 }
