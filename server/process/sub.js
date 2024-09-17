a3689c883c81060527f461148980f16bbba111ca
ejima_xx <060812ez.tm@gmail.com>
2024-09-18 00:13:41 +0900

    実行形式を子プロセスとしての起動に変更し、親との通信でパラメータや出力を送信する方式に変更、受信データ仕様の有無について分岐を実装

--- server/process/sub.js
+++ server/process/sub.js
@@ -1,17 +1,34 @@
 const { BoardData } = require("./functions/class");
-const yargs = require("yargs");
 
-process.on("message", m => {
-    console.log(m);
-})
+process.once("message", ({ problem, width, height, isGenQuestion, isSavedLog, isDrawnBoard}) => {
+    /**
+     * @type {BoardData}
+     */
+    let boardData;
+    
+    if (isSavedLog !== undefined && isDrawnBoard !== undefined) {
+        // 受信データを使用しない場合
+        if (width && height && isGenQuestion !== undefined) {
+            boardData = new BoardData(null, null, width, height);
+        }
+        // 受信データを使用する場合
+        else if (problem) {
+            boardData = new BoardData(problem.board, problem.general.patterns)
+        }
+    }
+    // エラーハンドラーを設定、ソート開始
+    boardData.answer.setErrorHandler((error) => {
+        process.send(error.stack);
+        process.exit(-1);
+    }).completeSort((match) => process.send(match));
+    // ファイル出力
+    boardData.writeReceptionData().writeSendData(true, (id, count) => process.send(`${id} ${count}`));
 
-const boardData = new BoardData(null, null, yargs.argv?.width, yargs.argv?.height);
+    process.exit();
+});
 
-boardData.answer.completeSort((match) => process.send(match));
 
-boardData.writeReceptionData().writeSendData(true, (id, count) => process.send(`${id} ${count}`));
 
-process.exit();
 /* コマンドライン引数のシグネチャ
  * node --max-old-space-size={RAM_SIZE[MB]} sub.js {params}
  * params: --width, --height, --isGenQuestion, --isSavedLog, --isDrawnBoard
