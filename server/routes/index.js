ebe3581b706b7d061869c931c58e4d9568c8757f
ejima_xx <060812ez.tm@gmail.com>
2024-09-18 00:18:29 +0900

    問題サーバーとの接続からプロセス実行まで実装

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,52 +6,83 @@ router.get('/', (req, res) => {
   res.render('index');
 });
 
+//#region sub.js 実行エンドポイント
+/** `sub.js`を起動する`ChildProcess */
 let sub = null;
 router.get("/start/:debug", async (req, res) => {
+  res.setHeader("Content-Type", "text/plain");
+  // デバッグモードがONの場合
   if (req.params.debug === "on") {
-    // sub = spawn("node", ["--max-old-space-size=16000", "process/sub.js", `--width=${req.query.width}`, `--height=${req.query.height}`]);
-    sub = fork("process/sub.js", [`--width=${req.query.width}`, `--height=${req.query.height}`]);
-    sub.send({test: "test"});
-    // sub.stdout.on("data", data => {
-    //   console.log(data.toString());
-    //   res.write(data.toString());
-    // });
-
-    // sub.stderr.on("data", data => {
-    //   console.error(data.toString());
-    //   res.write(data.toString());
-    // });
+    // 子プロセスを起動する --max-old-space-sizeは現状消してみている
+    sub = fork("process/sub.js");
+    // 子プロセス生成時、パラメータを送信する
+    sub.on("spawn", () => sub.send({
+      width: req.query.width,
+      height: req.query.height,
+      isGenQuestion: req.query.isGenQuestion,
+      isSavedLog: req.query.isSavedLog,
+      isDrawnBoard: req.query.isDrawnBoard
+    }));
+    // 子プロセスからのメッセージを受信後、レスポンスする
     sub.on("message", data => {
-      console.log(data.toString());
+      console.log(data);
       res.write(data.toString() + "\n");
+      res.flushHeaders();
     });
+    // エラーハンドリング
     sub.on("error", error => {
       console.error(error);
       res.write(error.toString());
     })
-
+    // プロセスが終了した後、レスポンスを終了させる
     sub.on("exit", () => res.end());
   }
+  // デバッグモードがOFFの場合
   else if (req.params.debug === "off") {
-    const headers = new Headers();
-    headers.append("Procon-Token", "kure3037997297c7e6e840bb98658ca5175aa607a40d7f59a343ff7ecf182c45");
+    // サーバーにFetchする（本番もLANならこれで通じる）
     await fetch(`http://localhost:${req.query.port}/problem`, {
       method: "GET",
       headers: { "Procon-Token": "kure3037997297c7e6e840bb98658ca5175aa607a40d7f59a343ff7ecf182c45" }
     }).then(response => {
       switch (response.status) {
+        // 正常に通信できた場合
         case 200:
-          response.json().then(obj => {
-            console.log(obj);
-            res.json(obj);
+          // レスポンスのJSONデータをパースする
+          response.json().then(data => {
+            // 手始めにボードサイズを送信する
+            res.write(`board: ${data.board.width}, ${data.board.height}\n`);
+            // 子プロセスを起動する
+            sub = fork("process/sub.js");
+            // 子プロセス生成時、パラメータを送信する
+            sub.on("spawn", () => sub.send({
+              problem: data,
+              isSavedLog: req.query.isSavedLog,
+              isDrawnBoard: req.query.isDrawnBoard
+            }));
+            // 子プロセスからのメッセージを受信後、レスポンスする
+            sub.on("message", data => {
+              console.log(data);
+              res.write(data.toString() + "\n");
+              res.flushHeaders();
+            });
+            // エラーハンドリング
+            sub.on("error", error => {
+              console.error(error);
+              res.write(error.toString());
+            })
+            // 子プロセスが終了した後、レスポンスを終了させる
+            sub.on("exit", () => res.end());
           });
-          return;
+          break;
+        // 403 Forbidden: サーバーは起動しているが試合がまだ開かれていない
         case 403:
           res.status(403).send("The match isn't holding now.");
           return;
+        // 401 Unauthorized: トークンが違う
         case 401:
           res.status(401).send("The token can be wrong.");
           return;
+        // その他予期しないエラーは500として送信する
         default:
           res.status(500).send("Unexpected error occured.");
           return;
@@ -59,42 +90,26 @@ router.get("/start/:debug", async (req, res) => {
     }).catch(error => res.status(500).send("Failed to connect server. Confirm runnnig status of server."));
   }
 })
+//#endregion
 
-router.post("/", (req, res) => {
-  // console.log(req.body.array);
-  // datas.push(req.body);
-  // res.redirect("/");
-})
-
+//#region 簡易サーバー起動エンドポイント
+/** `procon-server_win.exe`を起動するChildProcess */
 let proconServer = null;
-// router.get("/run-server/:flag", async (req, res) => {
-//   if (req.params.flag) {
-//     console.log(req.params.flag)
-//     proconServer = spawn("../simple-server/procon-server_win", ["-c", "../simple-server/input.json", "-l", ":8001", "-start", "0s"]);
-//     proconServer.stdout.on("data", data => {
-//       console.log(data.toString());
-//     })
-//     proconServer.stderr.on("data", data => {
-//       console.error("error", data.toString());
-//     })
-//   }
-//   else {
-//     console.log("dousite")
-//     proconServer?.kill();
-//   }
-
-// })
-
+// サーバーを起動させる
 router.get("/run-server/:port", (req, res) => {
+  // 受け取ったポート番号で起動後、確認メッセージを送信する
   proconServer = spawn("../simple-server/procon-server_win", ["-c", "../simple-server/input.json", "-l", `:${req.params.port}`, "-start", "0s"]);
   proconServer.on("spawn", () => res.send(`Procon simple server starts running at localhost:${req.params.port}.`));
 });
-
+// サーバーを停止させる
 router.get("/stop-server", (req, res) => {
+  // プロセスをキルする
   proconServer.on("exit", () => res.send("Procon simple server stopped running."));
-  proconServer?.kill();
+  proconServer.kill();
 })
+//#endregion
 
+//#region 処理結果表示ページエンドポイント
 router.get("/result", (req, res) => {
   let name = {
     question: `question_${req.query.id}.json`,
@@ -110,5 +125,6 @@ router.get("/result", (req, res) => {
     orders: data.result?.ops
   })
 });
+//#endregion
 
 module.exports = router;
