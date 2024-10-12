8d11e81d175100d8b840813845f8a53cd82d8e89
ejima_xx <060812ez.tm@gmail.com>
2024-10-12 19:51:06 +0900

    スワップ履歴の出力に対応

--- server/process/sub.js
+++ server/process/sub.js
@@ -15,21 +15,23 @@ process.once("message", ({ problem, width, height, isGenQuestion, isSavedLog, is
         else if (problem) {
             boardData = new BoardData(problem.board, problem.general.patterns)
         }
+        boardData.useSwapHistory(isDrawnBoard);
     }
     // エラーハンドラーを設定、ソート開始
     boardData.answer.setErrorHandler((error) => {
         process.send(error.stack);
         process.exit(-1);
-    }).completeSort((match) => process.send(match));
-    // ファイル出力
-    boardData.writeReceptionData().writeSendData(true, (id, count) => process.send(`${id} ${count}`));
+    }).completeSort((match) => process.send(match), () => process.send("sort starts"), (sec) => process.send("sort ended"));
+
+    /* isSavedLogの値を反転させることによって、
+     * 元々`true`=>`false`:「上書きしないで固有IDとともに保存
+     * 元々`false`=>`true`:「上書き保存」
+     */
+    boardData.writeSwapHistory(!isSavedLog).writeReceptionData(problem, !isSavedLog).writeSendData(!isSavedLog,
+        (id, answer) => {
+            process.send(`${id} ${answer.n}`);
+            if (problem) process.send(answer);
+        });
 
     process.exit();
 });
-
-
-
-/* コマンドライン引数のシグネチャ
- * node --max-old-space-size={RAM_SIZE[MB]} sub.js {params}
- * params: --width, --height, --isGenQuestion, --isSavedLog, --isDrawnBoard
- */
